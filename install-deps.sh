8af2ef09f14fbbb338cf3baaf7b084c80dc35787
Jeffrey Bush <jeff@coderforlife.com>
2024-05-14 12:05:48 -0400

    Added installer for wiring pi system library

--- install-deps.sh
+++ install-deps.sh
@@ -6,4 +6,11 @@ sudo usermod -a -G gpio $USER
 # Installs the system-wide dependencies for the project
 sudo apt install python3-dev
 
-# TODO: wiringPi C library
+# Install wiringPi C library
+version="3.4"
+arch=$(dpkg --print-architecture)
+name="wiringpi_${version}_${arch}.deb"
+wget "https://github.com/WiringPi/WiringPi/releases/download/$version/$name"
+sudo dpkg -i "$name"
+rm "$name"
+
